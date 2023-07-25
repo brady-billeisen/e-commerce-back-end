2f04b05cb0040dddf00ee116fced882d40799daa
brady <bradybill36@gmail.com>
2023-07-24 22:45:13 -0500

    got the get one product route done and working, as well as the delete a product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,6 +15,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, { include: Tag });
+
+    if (!product) {
+      return res.status(404).json('Product not found.')
+    };
+
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 // create new product
@@ -72,8 +84,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.findByPk(req.params.id);
+
+    if (!product) {
+      return res.status(404).json('Product not found.')
+    };
+
+    await product.destroy();
+
+    res.json('Product deleted.');
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
